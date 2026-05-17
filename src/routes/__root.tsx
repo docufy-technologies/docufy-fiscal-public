f0309a66a698cd15898c033506ac10d3500013d8
Rakibul Hasan Ratul <rakibulhasanratul@proton.me>
2026-05-17 22:43:09 +0600

    feat: implement footer component with social links and update routing

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,28 +1,30 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
-import '../styles.css'
+import "../styles.css";
+import Footer from "#/components/blocks/footer";
 
 export const Route = createRootRoute({
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <>
       <Outlet />
+      <Footer />
       <TanStackDevtools
         config={{
-          position: 'bottom-right',
+          position: "bottom-right",
         }}
         plugins={[
           {
-            name: 'TanStack Router',
+            name: "TanStack Router",
             render: <TanStackRouterDevtoolsPanel />,
           },
         ]}
       />
     </>
-  )
+  );
 }
